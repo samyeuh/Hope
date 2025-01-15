9a9e38f09e1e0cf8b8cd27df410b8aa2e8c0a8dd
samyeuh <samybouhamidi2@gmail.com>
2025-01-15 19:23:27 +0100

    SEARCH FUNCTION !!!!

--- src/main/java/Hope/model/DataRepository.java
+++ src/main/java/Hope/model/DataRepository.java
@@ -1,6 +1,22 @@
 package Hope.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNullApi;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface DataRepository extends JpaRepository<DataHope,Integer> {
+
+    @Query(value = "SELECT * FROM hopeDB.HOPE", nativeQuery = true)
+    List<DataHope> findAll();
+
+    @Query(value = "SELECT * FROM HOPE d WHERE " +
+            "LOWER(Titre) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+            "LOWER(Domaine) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+            "LOWER(Description_simple) LIKE LOWER(CONCAT('%', :query, '%'))", nativeQuery = true)
+    List<DataHope> search(@Param("query") String query);
 }
