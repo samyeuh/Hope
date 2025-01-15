81335bdaf75b6a4b59de8ea0bc0636bc64af3b6a
Matthieu Brandao <matthieu.brandao@efrei.net>
2025-01-15 22:38:23 +0100

    update feature and delete

--- src/main/java/Hope/controller/HopeService.java
+++ src/main/java/Hope/controller/HopeService.java
@@ -23,4 +23,8 @@ public class HopeService {
     }
 
     public Optional<DataHope> getData(int id){ return dataRepository.findById(id); }
+
+    public void deleteData(int id){ dataRepository.deleteById(id); }
+
+    public void updateData(DataHope data){ dataRepository.save(data); }
 }
